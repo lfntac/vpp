d0e2b68a3c74bacbc56e3bcacc343aeb0ac3da79
Ed Warnicke <hagbard@gmail.com>
2019-04-17 10:56:57 -0500

    Try using packet.net for runners
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ .circleci/terraform/packet.tf
@@ -0,0 +1,32 @@
+variable "auth_token" {}
+variable "project_id" {}
+variable "public_key" {}
+variable "master1_hostname" {}
+variable "worker1_1_hostname" {}
+variable "master2_hostname" {}
+variable "worker2_1_hostname" {}
+variable "public_key_name" {}
+
+provider "packet" {
+  auth_token = "${var.auth_token}"
+}
+
+resource "packet_ssh_key" "key" {
+  name       = "${var.public_key_name}"
+  public_key = "${file(var.public_key)}"
+}
+
+resource "packet_device" "worker" {
+  hostname         = "${var.master1_hostname}"
+  facilities       = ["sjc1"]
+  plan             = "t1.small.x86"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  depends_on       = ["packet_ssh_key.key"]
+}
+
+output "worker.public_ip" {
+  value = "${packet_device.master1.access_public_ipv4}"
+}
+
